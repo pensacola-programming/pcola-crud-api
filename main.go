f8b2e1faafe52b7d332d4af93510781e76007274
mulf <mhmulford0@gmail.com>
2024-06-15 12:32:21 -0500

    stub route handlers

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/google/uuid"
-
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 /*
@@ -28,11 +30,11 @@ type Todo struct {
 }
 
 type User struct {
-	Id        uuid.UUID
-	Email     string
+	Id        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
 	Password  string
-	FirstName string
-	LastName  string
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
 
 func main() {
@@ -41,10 +43,38 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	e.GET("/todo", handleGetTodo)
+	e.POST("/todo", handleAddTodo)
+
+	e.PUT("/todo/:id", handleUpdateTodo)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func handleAddTodo(c echo.Context) error {}
+func handleDeleteTodo(c echo.Context) error {
+	panic("Not implemented")
+}
+
+func handleAddTodo(c echo.Context) error {
+	id := c.Param("id")
+	fmt.Println(id)
+	panic("not implemented")
+}
+
+func handleUpdateTodo(c echo.Context) error {
+	panic("not implemented")
+}
+
+func handleGetTodo(c echo.Context) error {
+	t := &Todo{
+		Id:        uuid.New(),
+		Title:     "todo title",
+		Created:   time.Now(),
+		Due:       time.Now().AddDate(0, 0, 10),
+		Completed: false,
+		GroupId:   1,
+	}
+	return c.JSON(http.StatusOK, t)
+}
 
 /*
 POST to do list item /item
